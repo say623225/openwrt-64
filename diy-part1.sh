341f5b6f42e10317c678b818faf579b53ffc9506
say623225 <61894825+say623225@users.noreply.github.com>
2022-05-15 18:11:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,20 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+#  关机
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
+
+#ShadowSocksR Plus+
+git clone https://github.com/fw876/helloworld.git package/helloworld
+
+#passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+
+#passwall2
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-passwall2
+
+#openclash
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
