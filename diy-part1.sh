3c91fc1860c014f0f53dd3940cd37c8d70779455
say623225 <61894825+say623225@users.noreply.github.com>
2024-01-04 09:28:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,10 +19,6 @@
 
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-git pull
-./scripts/feeds update -a
-./scripts/feeds install -a
-make menuconfig
 
 # 主题
 git clone https://github.com/kenzok78/luci-theme-argonne.git package/luci-theme-argonne
